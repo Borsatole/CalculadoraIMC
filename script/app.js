40761bc8f7893e03120bb8fc2b0ea89ae9216fdb
F Santos <borsatole@gmailcom>
2024-03-20 11:59:58 -0300

    func calcular adicionada

--- script/app.js
+++ script/app.js
@@ -3,53 +3,38 @@ var btn_refazer = document.getElementById("refazer");
 var input_altura = document.getElementById("altura");
 var input_peso = document.getElementById("peso");
 
+var classecm = document.querySelectorAll(".cm");
+
 var animacao = document.querySelector(".grafico");
+var hstatus = document.querySelector(".status");
 var resultado = document.querySelector(".resultado");
 var niveistabela = document.querySelector(".niveis");
 
 var total = document.querySelector(".total");
-
-
-var resultadonb = 21.9;
-
-total.innerText = resultadonb;
-
 var status = "Normal";
 
+function calcular (){
+var imc = (input_peso.value / (input_altura.value * input_altura.value)*10000).toFixed(1);
+total.innerText = imc;
 
+if (imc < 16.9) {
+    status = "Muito abaixo do peso"
+}else if(imc < 18.4) {
+    status = "Abaixo do peso"
+}else if(imc < 24.9) {
+    status = "Normal";
+}else if(imc < 29.9) {
+    status = "Sobrepeso"
+}else if(imc < 34.9) {
+    status = "Obesidade Grau 1"
+}else if(imc < 39.9) {
+    status = "Obesidade Grau 2"
+}else if(imc > 40) {
+    status = "Obesidade Grau 3"
+}
 
-
-btn_calcular.addEventListener("click",()=>{
-    var input_altura = document.getElementById("altura");
-    var input_peso = document.getElementById("peso");
-
-    if(input_altura.value == ""|| input_altura.value <= 0) {
-
-        input_altura.value = 1;
-        input_peso.value = 0;
-        alert("Verifique os dados e tente novamente");
-
-    } else {
-        animacao.style.display = "none";
-        resultado.style.display = "flex";
-        niveistabela.style.display = "flex";
-        btn_calcular.style.display = "none";
-        btn_refazer.style.display = "block";
-    }
-});
-
-btn_refazer.addEventListener("click",()=>{
-        input_altura.value = 1;
-        input_peso.value = 0;
-        animacao.style.display = "block";
-        resultado.style.display = "none";
-        niveistabela.style.display = "none";
-        btn_calcular.style.display = "block";
-        btn_refazer.style.display = "none";
-})
-
-
-
+hstatus.innerHTML = `${status}<br>
+<span id=""><i class='bx bxs-check-circle'></i>IMC Calculado!</span>`
 
         animacao.style.display = "none";
         resultado.style.display = "flex";
@@ -67,3 +52,45 @@ btn_refazer.addEventListener("click",()=>{
                 linhas[i].style.color = "white";
             }
         }
+
+
+        classecm[0].innerHTML = `<input type="number" id="altura" disabled >
+        <label for="">cm</label>`;
+
+        classecm[1].innerHTML = `<input type="number" id="peso" disabled>
+        <label for="">kg</label>`;
+
+        console.log(input_peso.value/(input_altura.value * input_altura.value));
+
+};
+
+
+btn_calcular.addEventListener("click",()=>{
+    var input_altura = document.getElementById("altura");
+    var input_peso = document.getElementById("peso");
+
+    if(input_altura.value == ""|| input_altura.value <= 0) {
+
+        input_altura.value = 0;
+        input_peso.value = 0;
+        alert("Verifique os dados e tente novamente");
+
+    } else {
+        calcular();
+    }
+});
+
+btn_refazer.addEventListener("click",()=>{
+    window.location.reload(true);
+})
+
+
+
+
+        
+
+        
+        
+
+
+        
